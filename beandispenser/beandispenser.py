330d68d4eda3835b74b5ff18b0786ced686f5393
Willem van der Jagt <wkjagt@gmail.com>
2014-09-29 22:42:50 -0400

    config value validation

--- beandispenser/beandispenser.py
+++ beandispenser/beandispenser.py
@@ -10,12 +10,42 @@ import signal
 from ConfigParser import ConfigParser, NoOptionError
 import syslog
 
+class ConfigValueError(Exception):
+    pass
+
+class ConfigValue(object):
+
+    def __init__(self, section, key, value):
+
+        if key[:3] == 'on_' and value not in ['delete', 'bury', 'release']:
+            raise ConfigValueError('[{0} {1}] value must be on of delete, bury, release'.format(section, key))
+
+        self._section = section
+        self._key = key
+        self._value = value
+
+    def __int__(self):
+        if self._key in ['workers', 'port']:
+            return int(self._value)
+        raise ConfigValueTypeError()
+
+    def __str__(self):
+        return self._value
+
+    def __repr__(self):
+        return self._value        
+
+    def __unicode__(self):
+        return self._value
+
+
 class Config(ConfigParser):
     """Just a simple way of getting None back if the config doesn't exist
     """
     def get(self, section, key, default=None):
         try:
-            return ConfigParser.get(self, section, key)
+            value = ConfigParser.get(self, section, key)
+            return ConfigValue(section, key, value)
         except NoOptionError:
             return None
 
@@ -117,7 +147,7 @@ class Worker(Logger, object):
 
         try:
             self._beanstalk = beanstalkc.Connection(
-                host=connection['host'],
+                host=str(connection['host']),
                 port=connection['port'])
         except beanstalkc.SocketError:
             self.error("Failed to connect. Retrying in 5 seconds")
@@ -165,7 +195,8 @@ class Worker(Logger, object):
                     handler = self._fail_handlers['on_tempory_fail']
                 else:
                     handler = self._fail_handlers['on_unknown_fail']
-                getattr(job, handler)()
+
+                getattr(job, str(handler))()
 
             except TimeOut:
                 self.info("job {0} timed out", job.stats()['id'])
@@ -210,7 +241,7 @@ class Command(object):
         param timeout: the time allowed to run
         param input:   the content to pass to the command's stdin
         """ 
-        self.command = shlex.split(command)
+        self.command = shlex.split(str(command))
         self.timeout = timeout
         self.input = input
  
@@ -302,7 +333,7 @@ class Forker(Logger, object):
                 "on_timeout" : config.get(section, "on_timeout"),
                 "connection" : {
                     "host" : config.get('connection', 'host'),
-                    "port" : int(config.get('connection', 'port'))
+                    "port" : config.get('connection', 'port')
                 }
             }
 
@@ -319,7 +350,7 @@ class Forker(Logger, object):
 
         for pool in self._pools:
 
-            for i in range(int(pool["worker_count"])):
+            for i in range(pool["worker_count"]):
 
                 # fork the current process. The parent and the child both continue
                 # from this point so we need to make sure that only the child
