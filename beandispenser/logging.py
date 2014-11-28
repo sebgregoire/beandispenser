6abe0be668e8b2e57d1f76c49cfaee0798e8ae74
Willem van der Jagt <wkjagt@gmail.com>
2014-11-28 15:48:12 -0500

    make it easier to grep logs

--- beandispenser/logging.py
+++ beandispenser/logging.py
@@ -3,9 +3,9 @@ import sys
 class Logger(object):
 
     def info(self, msg, *args):
-        sys.stdout.write(msg.format(*args) + "\n")
+        sys.stdout.write("beandispenser info: " + msg.format(*args) + "\n")
         sys.stderr.flush()
 
     def error(self, msg, *args):
-        sys.stderr.write(msg.format(*args) + "\n")
+        sys.stderr.write("beandispenser error: " + msg.format(*args) + "\n")
         sys.stderr.flush()
