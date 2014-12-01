e9591d66aca17a8ea998827b37e4645fbea07b9b
Willem van der Jagt <wkjagt@gmail.com>
2014-12-01 11:22:08 -0500

    put pid in logs

--- beandispenser/logging.py
+++ beandispenser/logging.py
@@ -1,11 +1,11 @@
-import sys
+import sys, os
 
 class Logger(object):
 
     def info(self, msg, *args):
-        sys.stdout.write("beandispenser info: " + msg.format(*args) + "\n")
-        sys.stderr.flush()
+        sys.stdout.write("beandispenser (pid {}) info: {} \n".format(os.getpid(), msg.format(*args)))
+        sys.stdout.flush()
 
     def error(self, msg, *args):
-        sys.stderr.write("beandispenser error: " + msg.format(*args) + "\n")
+        sys.stdout.write("beandispenser (pid {}) error: {} \n".format(os.getpid(), msg.format(*args)))
         sys.stderr.flush()
